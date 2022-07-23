d40112eaaf0032d5d70b7291722a74721ef6e044
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2022-07-24 01:14:47 +0200

    Changed cms template

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -1,111 +1,85 @@
-import {CreditCardIcon, EyeOpenIcon, DocumentIcon} from '@sanity/icons'
-import {ProductPreview} from '../components/views/ProductPreview'
-import S from '@sanity/desk-tool/structure-builder'
-import {JsonView} from '../components/views/JsonView'
-import productIcon from '../components/icons/productIcon'
-
 export default {
   name: 'product',
-  type: 'document',
   title: 'Product',
-  icon: productIcon,
-  views: [
-    S.view.component(ProductPreview).title('Preview').icon(EyeOpenIcon),
-    S.view.component(JsonView).title('JSON').icon(DocumentIcon),
-  ],
-  preview: {
-    select: {
-      title: 'name',
-      media: 'variants.0.picture',
-      variants: 'variants',
-    },
-    prepare({title, media, variants = []}) {
-      const priceRangeMin = Math.min(...Object.values(variants).map((variant) => variant.price))
-      const priceRangeMax = Math.max(...Object.values(variants).map((variant) => variant.price))
-      const singlePrice = variants?.[0]?.price
-      const priceRange =
-        priceRangeMax > priceRangeMin
-          ? `$${priceRangeMin} - $${priceRangeMax}`
-          : singlePrice
-          ? `$${priceRangeMax}`
-          : 'No price set'
-
-      return {
-        title,
-        media,
-        subtitle: priceRange,
-      }
-    },
-  },
+  type: 'document',
   fields: [
     {
-      name: 'name',
+      name: 'title',
+      title: 'Title',
       type: 'string',
-      title: 'Name',
     },
     {
-      name: 'tagline',
-      type: 'string',
-      title: 'Tagline',
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
     },
     {
-      name: 'material',
-      type: 'string',
-      title: 'Material',
+      title: 'Default variant',
+      name: 'defaultProductVariant',
+      type: 'productVariant',
     },
     {
-      name: 'description',
-      title: 'Description',
-      type: 'array',
-      of: [{type: 'block'}],
-    },
-    {
-      name: 'variants',
-      description:
-        'This is an array of objects. Unlike references, objects are always a part of a parent document.',
       title: 'Variants',
+      name: 'variants',
       type: 'array',
       of: [
         {
-          type: 'object',
-          fields: [
-            {
-              name: 'name',
-              type: 'string',
-              title: 'Name',
-            },
-            {
-              name: 'price',
-              type: 'number',
-              title: 'Price (USD)',
-            },
-            {
-              name: 'size',
-              type: 'array',
-              of: [{type: 'string'}],
-              title: 'Size',
-            },
-            {
-              name: 'picture',
-              type: 'image',
-              title: 'Picture',
-              options: {
-                hotspot: true, // <-- Defaults to false
-              },
-              fields: [
-                {
-                  name: 'caption',
-                  type: 'string',
-                  title: 'Caption',
-                  options: {
-                    isHighlighted: true, // <-- make this field easily accessible
-                  },
-                },
-              ],
-            },
-          ],
+          title: 'Variant',
+          type: 'productVariant',
         },
       ],
     },
+    {
+      title: 'Tags',
+      name: 'tags',
+      type: 'array',
+      of: [
+        {
+          type: 'string',
+        },
+      ],
+      options: {
+        layout: 'tags',
+      },
+    },
+    {
+      name: 'vendor',
+      title: 'Vendor',
+      type: 'reference',
+      to: {type: 'vendor'},
+    },
+    {
+      name: 'blurb',
+      title: 'Blurb',
+      type: 'localeString',
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: {type: 'category'},
+        },
+      ],
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'localeBlockContent',
+    },
   ],
+
+  preview: {
+    select: {
+      title: 'title',
+      manufactor: 'manufactor.title',
+      media: 'defaultProductVariant.images[0]',
+    },
+  },
 }
