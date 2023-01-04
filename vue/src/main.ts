28e8badc2d2827bfc9886006a0518138495803de
Srdjan Damnjanovic <srdjan.damnjanovic@msg-global.com>
2023-01-04 22:13:26 +0100

    MB-2: removed unused code

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-// import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel';
 import App from './App.vue';
 import router from './router';
 import './index.css';
@@ -8,9 +7,5 @@ import './index.css';
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
-// app.component('Carousel', Carousel);
-// app.component('Navigation', Navigation);
-// app.component('Pagination', Pagination);
-// app.component('Slide', Slide);
 
 app.mount('#app');
