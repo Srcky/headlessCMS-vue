11d9c667b7cf95568714182ec7ce6165aa8e5b5c
Srdjan Damnjanovic <srdjan.damnjanovic@impactcommeerce.se>
2023-06-28 18:05:59 +0200

    EXP-MB-3: added PG

--- cms/config/env/production/database.js
+++ cms/config/env/production/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
       ssl: env.bool("DATABASE_SSL", false) && {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        ca: env("DATABASE_CA"),
       },
     },
     debug: false,
