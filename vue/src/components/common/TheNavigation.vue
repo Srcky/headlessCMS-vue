296980ba5392d2c9111e0cc19e0f8a4b6084101b
Srdjan Damnjanovic <srdjan.damnjanovic@impactcommeerce.se>
2023-10-05 21:27:36 +0200

    Handled menu outside click

--- vue/src/components/common/TheNavigation.vue
+++ vue/src/components/common/TheNavigation.vue
@@ -14,13 +14,22 @@ const toggleNav = () => {
     navOpen.value = !navOpen.value;
 };
 
+// Close navigation on click outside menu
+const handleClickOutside = (evt: Event) => {
+    if (!(evt.target as HTMLElement)?.dataset?.menu) {
+        navOpen.value = false;
+    }
+};
+
 // Ensuring no scroling is possible when mobile menu is opened
 watch(navOpen, (newNavOpen) => {
     const bodyElement = document.body;
     if (newNavOpen) {
         bodyElement.classList.add('overflow-hidden');
+        document.addEventListener('touchend', handleClickOutside);
     } else {
         bodyElement.classList.remove('overflow-hidden');
+        document.removeEventListener('touchend', handleClickOutside);
     }
 });
 
@@ -31,15 +40,14 @@ watch(route, (newPage) => {
     }
 });
 
-
 </script>
 <template>
     <span class="material-icons-outlined absolute left-4 text-white top-1/2 z-40 -translate-y-1/2 md:hidden"
-        @click="toggleNav">
+        @touchend="toggleNav" data-menu="true">
         {{ navOpen ? 'close' : 'menu' }}
     </span>
     <nav class="fixed h-screen w-[90vw] top-0 pt-20 pl-4 left-0 -translate-x-full transition-transform duration-300 ease-in bg-blue-600 md:pt-0 md:pl-0 md:relative md:h-auto md:w-auto md:bg-inherit md:translate-x-0"
-        :class="{ 'translate-x-0': navOpen }" aria-label="Primary navigation">
+        :class="{ 'translate-x-0': navOpen }" aria-label="Primary navigation" data-menu="true">
         <ul class="flex flex-wrap justify-center gap-7 flex-col md:flex-row">
             <li v-for="item in navigationItems" :key="item.title" class="flex">
                 <router-link
